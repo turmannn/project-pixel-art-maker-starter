9286bf78ab1f703e4cea69576cd35b97315c316c
artb <temiy@tut.by>
2022-12-12 20:50:43 -0500

    feat: add app logic
    single-page web app that allows users to draw pixel art on a customizable canvas

--- designs.js
+++ designs.js
@@ -1,10 +1,36 @@
-// Select color input
-// Select size input
+const eltColorPicker = document.getElementById('colorPicker');
+const eltSizePickerSubmit = document.getElementById('sizePicker');
+const eltTable = document.getElementById('pixelCanvas');
+const eltGridHeight = document.getElementById('inputHeight');
+const eltGridWidth = document.getElementById('inputWidth');
 
-// When size is submitted by the user, call makeGrid()
+// 
+eltSizePickerSubmit.addEventListener('submit', function(e) {
+    e.preventDefault()  
+    makeGrid();
+})
+
+function gridSquareListener(e) {
+    e.target.style.backgroundColor = eltColorPicker.value
+}
 
 function makeGrid() {
+    const gridWidth = eltGridWidth.value;
+    const gridHeight = eltGridHeight.value;
 
-// Your code goes here!
-
+    // remove children elements of the table
+    while (eltTable.firstChild) {
+        eltTable.firstChild.remove();
+    }
+    
+    // create grid to the table
+    for (var row = 0; row < gridWidth; row++) {
+        const eltRow = document.createElement('TR');
+        for (var col = 0; col < gridHeight; col++) {
+            const eltCol = document.createElement('TD');
+            eltCol.addEventListener('click', gridSquareListener);
+            eltRow.appendChild(eltCol);
+        }
+        eltTable.appendChild(eltRow);
+    }
 }
